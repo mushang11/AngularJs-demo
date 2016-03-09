8f0b706a745d223883de5ef0c0192afa9f2a0862
watcher <mushangwq@163.com>
2016-03-09 17:36:37 +0800

    Update initModel.js

--- initModel.js
+++ initModel.js
@@ -1,4 +1,10 @@
 <--init the dictionary data-->
 <--dataType:json-->
 demo.constant('dictionary',{
+  name:[
+    {value:'1',text:'wade'},
+    {value:'2',text:'bosh'},
+    {value:'3',text:'james'},
+    {value:'4',text:'kobe'},
+    ];
 });
