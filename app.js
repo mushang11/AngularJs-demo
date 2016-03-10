7e1fc2a50f04b1614aeeadee3a0774f937564d6a
watcher <mushangwq@163.com>
2016-03-10 16:44:44 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 <--angular file inits first-->
 angular.module('demo',[]);
-zpmanage.config(['routeProvider','$locationProvider','httpProvider',function($routeProvider,$locationProvider,$httpProvider){
-$routeProvider.when();
+demo.config(['routeProvider','$locationProvider','httpProvider',function($routeProvider,$locationProvider,$httpProvider){
+$routeProvider.when("/index",{templateUrl:'',controller:'',pageTitle:''})
+.when('/demo',{templateUrl:'',controller:'',pageTitle:''})
+.other({'/noPrivilege'});
 $locationProvider.html5Mode(true);
 }
 ]);
+demo.run(['$rootScope','$location','$window','$http','$filter',function($rootScope,$location,$window,$http,$filter){
+  // route changes successfully.
+  $rootScope.$on('$routeChangeSuccess',function(next,current){
+    if(current && current.$$route && current.$$route.pageTitle){
+      // your own code...
+    }
+  });
+  //route starts privilege limit
+  $rootScope.$on('$routeChangeStart',function(event,next,current){
+    //privilege control
+  });
+  
+  $rootScope.$on('$routeChangeError',function(){
+    // route changes errorly!
+  })
+  
+}]);
+
