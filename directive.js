ed0c21b4900059a220672fdf8958bd50300c9fe9
mushang11 <mushangwq@163.com>
2016-03-11 13:30:42 +0800

    directive

--- directive.js
+++ directive.js
@@ -1,7 +1,26 @@
-demo.directive('demo',function(){
+demo.directive('hasPermission',function(permissions){
 	return{
 		restrict:'EA',
 		replace:true,
-		template:"<div>i am a good worker!!!</div>"
+		link: function(scope,element,attrs){
+			if(!_.isString(attrs.hasPermission)){
+				throw "The hasPermission value must be a string";
+			}
+			var value = attrs.hasPermission.trim();
+			var notPermissionFlag = value[0] === '!';
+			if （notPermissionFlag）{
+				value  = value.slice(1).trim();
+			}
+			function toggleVisibilityBasedOnPermission(){
+				var hasPermission = permissions.hasPermission(value);
+
+				if(hasPermission && !notPermissionFlag || !hasPermission && notPermissionFlag)
+					element.show();
+				else
+					element.hide();
+			}
+            toggleVisibilityBasedOnPermission();
+            scope.$on('permissionsChanged',toggleVisibilityBasedOnPermission);
+		}
 	}
 });
